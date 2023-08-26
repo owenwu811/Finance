9a17adb310a9e04e6ae21490eac609817c5b4291
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2023-08-26 07:21:58 -0700

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -1,7 +1,6 @@
 import os
 import requests
 import urllib.parse
-
 from flask import redirect, render_template, request, session
 from functools import wraps
 
@@ -39,13 +38,10 @@ def lookup(symbol):
 
     # Contact API
     try:
-        api_key = os.environ.get("API_KEY")
-        url = f"https://cloud.iexapis.com/stable/stock/{urllib.parse.quote_plus(symbol)}/quote?token={api_key}"
-        response = requests.get(url)
+        api_key, url, response = os.environ.get("API_KEY"), f"https://cloud.iexapis.com/stable/stock/{urllib.parse.quote_plus(symbol)}/quote?token={api_key}", requests.get(url)
         response.raise_for_status()
     except requests.RequestException:
         return None
-
     # Parse response
     try:
         quote = response.json()
